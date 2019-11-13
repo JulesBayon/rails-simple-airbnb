8eb4018b3e25ae27678a2dc3deb46bdff2d9eede
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-11-13 20:41:15 +0100

    Making sure I saved everything

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Modern Appartment in Tsim Sha Tsui',
+    address: 'Pok Fu Lam Road',
+    description: 'Fancy terrace flat for family or friends.',
+    price_per_night: 75,
+    number_of_guests: 5
+  },
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat.',
+    price_per_night: 50,
+    number_of_guests: 2
+  },
+  {
+    name: 'Authentic Hong Konger appartment in Choi Hung',
+    address: 'Choi Hung Estate',
+    description: 'Iconest place with awesome basketball courts.',
+    price_per_night: 47,
+    number_of_guests: 2
+  },
+  {
+    name: 'Expatriate condominium in Mid-Levels Central',
+    address: 'Central',
+    description: "Lot of money you don't know how to spend ?.",
+    price_per_night: 5000,
+    number_of_guests: 2
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
